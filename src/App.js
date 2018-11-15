2adb418bf2dd72630e24b4cf24a48e62b5a58c12
Kenneth F Cannon <kennethfcannon@gmail.com>
2018-11-15 07:20:51 -0500

    Stuck

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import Color from './Color'
 
 class App extends Component {
   render() {
+    let colors = [
+      {
+        value: 0
+      },
+      {
+        value: 0
+      },
+      {
+        value: 0
+      }
+    ]
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>COLOR PICKER</h1>
         </header>
+        <main>
+          {colors.map((color, index) => (
+            <Color
+              key={index}
+              value={this.state.value}
+              onChange={this.updateColorValue}
+            />
+          ))}
+
+          <button type="button">Click Me!</button>
+
+          {/* <p>{this.state.}</p> */}
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
