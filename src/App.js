63214c59b7325b5d840947ed76d76f4c4b03284d
Kenneth F Cannon <kennethfcannon@gmail.com>
2018-11-25 21:15:01 -0500

    Done. May style more later.

--- src/App.js
+++ src/App.js
@@ -5,35 +5,10 @@ import Color from './Color'
 
 class App extends Component {
   render() {
-    let colors = [
-      {
-        value: 0
-      },
-      {
-        value: 0
-      },
-      {
-        value: 0
-      }
-    ]
     return (
-      <div className="App">
-        <header>
-          <h1>COLOR PICKER</h1>
-        </header>
-        <main>
-          {colors.map((color, index) => (
-            <Color
-              key={index}
-              value={this.state.value}
-              onChange={this.updateColorValue}
-            />
-          ))}
-
-          <button type="button">Click Me!</button>
-
-          {/* <p>{this.state.}</p> */}
-        </main>
+      <div>
+        <Color />
+        <footer>Use the Sliders to Select Your Color</footer>
       </div>
     )
   }
--- src/Color.js
+++ src/Color.js
@@ -5,121 +5,150 @@ class Color extends Component {
     super(props)
 
     this.state = {
-      colorValue: props.colorValue
+      hue: 180,
+      saturation: 50,
+      lightness: 50
     }
   }
 
-  updateColorValue = event => {
+  updateHueValue = event => {
     console.log(event.target.value)
 
     this.setState({
-      colorValue: event.target.value()
+      hue: event.target.value
+    })
+  }
+
+  updateSaturationValue = event => {
+    console.log(event.target.value)
+
+    this.setState({
+      saturation: event.target.value
+    })
+  }
+
+  updateLightnessValue = event => {
+    console.log(event.target.value)
+
+    this.setState({
+      lightness: event.target.value
     })
   }
 
   render() {
+    let color = `hsl(${this.state.hue},${this.state.saturation}%,${
+      this.state.lightness
+    }%)`
     return (
-      <section>
-        <button type="button" onChange={this.state.colorValue} />
+      <div
+        className="color-picker"
+        style={{
+          backgroundColor: color
+        }}
+      >
+        <header>
+          <h1>Pick A Color</h1>
+        </header>
+        <main>
+          <section class="flex">
+            <section class="slider">
+              <input
+                type="range"
+                value={this.state.hue}
+                onChange={this.updateHueValue}
+                min="0"
+                max="360"
+                list="tickmarks"
+              />
+              <datalist id="tickmarks">
+                <option value="0" />
+                <option value="10" />
+                <option value="20" />
+                <option value="30" />
+                <option value="40" />
+                <option value="50" />
+                <option value="60" />
+                <option value="70" />
+                <option value="80" />
+                <option value="90" />
+                <option value="100" />
+                <option value="110" />
+                <option value="120" />
+                <option value="130" />
+                <option value="140" />
+                <option value="150" />
+                <option value="160" />
+                <option value="170" />
+                <option value="180" />
+                <option value="190" />
+                <option value="200" />
+                <option value="210" />
+                <option value="220" />
+                <option value="230" />
+                <option value="240" />
+                <option value="250" />
+                <option value="260" />
+                <option value="270" />
+                <option value="280" />
+                <option value="290" />
+                <option value="300" />
+                <option value="310" />
+                <option value="320" />
+                <option value="330" />
+                <option value="340" />
+                <option value="350" />
+                <option value="360" />
+              </datalist>
+              <label for="hue">Hue</label>
+              <input
+                type="range"
+                value={this.state.saturation}
+                onChange={this.updateSaturationValue}
+                min="0"
+                max="100"
+                list="tickmarks"
+              />
+              <datalist id="tickmarks">
+                <option value="0" />
+                <option value="10" />
+                <option value="20" />
+                <option value="30" />
+                <option value="40" />
+                <option value="50" />
+                <option value="60" />
+                <option value="70" />
+                <option value="80" />
+                <option value="90" />
+                <option value="100" />
+              </datalist>
 
-        <input
-          type="range"
-          value={this.state.colorValue}
-          onChange={this.updateColorValue}
-          min="0"
-          max="360"
-          list="tickmarks"
-        />
-        <datalist id="tickmarks">
-          <option value="0" />
-          <option value="10" />
-          <option value="20" />
-          <option value="30" />
-          <option value="40" />
-          <option value="50" />
-          <option value="60" />
-          <option value="70" />
-          <option value="80" />
-          <option value="90" />
-          <option value="100" />
-          <option value="110" />
-          <option value="120" />
-          <option value="130" />
-          <option value="140" />
-          <option value="150" />
-          <option value="160" />
-          <option value="170" />
-          <option value="180" />
-          <option value="190" />
-          <option value="200" />
-          <option value="210" />
-          <option value="220" />
-          <option value="230" />
-          <option value="240" />
-          <option value="250" />
-          <option value="260" />
-          <option value="270" />
-          <option value="280" />
-          <option value="290" />
-          <option value="300" />
-          <option value="310" />
-          <option value="320" />
-          <option value="330" />
-          <option value="340" />
-          <option value="350" />
-          <option value="360" />
-        </datalist>
-
-        <label for="hue">Hue</label>
-
-        <input
-          type="range"
-          value={this.state.colorValue}
-          onChange={this.updateColorValue}
-          min="0"
-          max="100"
-          list="tickmarks"
-        />
-        <datalist id="tickmarks">
-          <option value="0" />
-          <option value="10" />
-          <option value="20" />
-          <option value="30" />
-          <option value="40" />
-          <option value="50" />
-          <option value="60" />
-          <option value="70" />
-          <option value="80" />
-          <option value="90" />
-          <option value="100" />
-        </datalist>
-
-        <label for="saturation">Saturation</label>
-
-        <input
-          type="range"
-          value={this.state.colorValue}
-          onChange={this.updateColorValue}
-          min="0"
-          max="100"
-          list="tickmarks"
-        />
-        <datalist id="tickmarks">
-          <option value="0" />
-          <option value="10" />
-          <option value="20" />
-          <option value="30" />
-          <option value="40" />
-          <option value="50" />
-          <option value="60" />
-          <option value="70" />
-          <option value="80" />
-          <option value="90" />
-          <option value="100" />
-        </datalist>
-
-        <label for="lightness">Lightness</label>
-      </section>
+              <label for="saturation">Saturation</label>
+              <input
+                type="range"
+                value={this.state.lightness}
+                onChange={this.updateLightnessValue}
+                min="0"
+                max="100"
+                list="tickmarks"
+              />
+              <datalist id="tickmarks">
+                <option value="0" />
+                <option value="10" />
+                <option value="20" />
+                <option value="30" />
+                <option value="40" />
+                <option value="50" />
+                <option value="60" />
+                <option value="70" />
+                <option value="80" />
+                <option value="90" />
+                <option value="100" />
+              </datalist>
+              <label for="lightness">Lightness</label>
+            </section>
+          </section>
+        </main>
+      </div>
     )
   }
 }
